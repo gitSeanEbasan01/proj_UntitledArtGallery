f24c04ac1ae21ae845956951a12dc349d227666b
HeadphoneUserDev <ebasansean@gmail.com>
2022-06-25 07:45:11 +0800

    HTML & CSS: Navigation Area.

--- site00/js/navBar.js
+++ site00/js/navBar.js
@@ -2,19 +2,39 @@ const   navcExplore = document.querySelector('.navcExplore'),
         navcAbout = document.querySelector('.navcAbout'),
 
         navcBackground = document.querySelector('.navCategoryBackground'),
-        navNegative = document.querySelector('.navBarNegative');
+        navNegative = document.querySelector('.navBarNegative'),
+        
+        navInfo = document.querySelector('.navInfo'),
+        navInfoNegative = document.querySelector('.navInfoNegative');
 
 navcExplore.addEventListener('click', clickExplore);
-navcAbout.addEventListener('click', clickExplore);
+navcAbout.addEventListener('click', clickAbout);
+
 navNegative.addEventListener('click', clickNegative);
+navInfoNegative.addEventListener('click', clickNegative);
 
 function clickExplore(){
     navcBackground.classList.add('active');
     navNegative.classList.add('active');
+
+    navInfo.classList.add('active');
+    navInfoNegative.classList.add('active');
 };
+function clickAbout(){
+    navcBackground.classList.add('active');
+    navNegative.classList.add('active');
+
+    navInfo.classList.add('active');
+    navInfoNegative.classList.add('active');
+}
+
+
+
 
 function clickNegative(){
     navcBackground.classList.remove('active');
     navNegative.classList.remove('active');
-    console.log("clicked");
+    
+    navInfo.classList.remove('active');
+    navInfoNegative.classList.remove('active');
 };
