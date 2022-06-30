9a414441b6934df4aa8a2d1e3e70e6c3293bd911
HeadphoneUserDev <ebasansean@gmail.com>
2022-06-30 12:49:52 +0800

    JavaScript: Did some small changes to the intersection observer.

--- site00/js/beginningEnd.js
+++ site00/js/beginningEnd.js
@@ -1,12 +1,13 @@
-let betsTitle = document.querySelector('.titleSection'),
+let beTitleSection = document.querySelector('.titleSection'),
     beCurrentStudies = document.querySelector('.currentStudies'),
+    betsTitle = document.querySelector('.tsTitle'),
 
     beNavigationArea = document.querySelector('.navArea'),
     beOverallContainer = document.querySelector('.overallContainer');
 
 let option00 = {
-    rootMargin00: '0px',
-    threshold00: .5
+    rootMargin: '0px',
+    threshold: 1
 };
 
 let setItemActive00 = (entries => {
@@ -14,9 +15,18 @@ let setItemActive00 = (entries => {
 
     entries.forEach(entry => {
         if(entry.isIntersecting){
-            if(entry.target.classList.contains('currentStudies')){
+            if(entry.target.classList.contains('tsTitle')){
+                beOverallContainer.classList.remove('active');
+                beTitleSection.classList.remove('active');
+
+                beNavigationArea.classList.remove('active');
+            }
+            
+        }
+        else{
+            if(entry.target.classList.contains('tsTitle')){
                 beOverallContainer.classList.add('active');
-                betsTitle.classList.add('active');
+                beTitleSection.classList.add('active');
 
                 beNavigationArea.classList.add('active');
             }
@@ -26,4 +36,5 @@ let setItemActive00 = (entries => {
 
 let observer00 = new IntersectionObserver(setItemActive00, option00);
 
-beCurrentStudies = observer00.observe(beCurrentStudies);
+// beCurrentStudies = observer00.observe(beCurrentStudies);
+betsTitle = observer00.observe(betsTitle);
